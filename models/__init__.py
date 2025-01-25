6ab1d0bc230aaeaebbbb2fd74ff77dd7b9189b49
pedroroblesduten <pedroroblesduten@gmail.com>
2025-01-25 20:29:19 +0000

    small fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 from .vit import VisionTransformer
 from .vit_predictor import VisionTransformerPredictor, VisionTransformerCrossPredictor
-from .mine_predictor import MutualInformationPredictor, MutualInformationCrossPredictor
-from .mine_estimator import MutualInformationEstimator
 from .vic_reg import VICReg
 from .sem import SEM
 from .utils import repeat_interleave_batch, apply_masks, FullGatherLayer, Projector
